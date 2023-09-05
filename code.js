cb42a884e59431b0b4c6d5d8805feff442d3072e
asbrii <srinivrs222@gmail.com>
2023-09-05 11:46:47 +0530

    final commit

---
+++ code.js
@@ -0,0 +1,13 @@
+const profile = document.getElementById("clicker");
+
+const login = document.getElementById("login")
+
+profile.addEventListener("click", function dd() {
+        login.style.display='flex';
+})
+
+const offers = document.getElementById("pro1");
+const productPopup = document.getElementById("product");
+offers.addEventListener("click", function dd1() {
+    productPopup.style.display='flex';
+})
